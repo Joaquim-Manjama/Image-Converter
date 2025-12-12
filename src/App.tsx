832a94894a2a4c68dfb65a6a34cc37039ec83f16
Joaquim Manjama <melorafaelmanjama@gmail.com>
2025-12-12 19:44:15 +0000

    improved UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import { useState } from "react";
 
 function App() {
 
-  const [supportedImageTypes, setSupportedImageTypes] = useState([
-    ["BMP", "Uncompressed", false],
-    ["GIF", "Animation Support", false],
-    ["JPG", "Small file size", false],
-    ["PNG", "Lossless, transparent", false],
-    ["PPM", "Binary", false],
-    ["WEBP", "Modern, efficient", false],
-    
-    
-  ]);
+  const [selectedOutputFormat, setSelectedOutputFormat] = useState("");
+  const [fileUploaded, setFileUploaded] = useState(false);
 
-  const handleFormatChoice = (imageType: string | boolean): void => {
-    setSupportedImageTypes(prev => [prev.map(type => imageType == type[0] ? type[2] = true : type[2] = false)]);
+  const supportedImageTypes: string[][] = [
+    ["BMP", "Uncompressed"],
+    ["GIF", "Animation Support"],
+    ["JPG", "Small file size"],
+    ["PNG", "Lossless, transparent"],
+    ["PPM", "Binary"],
+    ["WEBP", "Modern, efficient"],
+  ];
+
+  const handleFormatChoice = (imageType: string): void => {
+    setSelectedOutputFormat(imageType);
   }
 
   return (
@@ -41,12 +42,12 @@ function App() {
         <div className="p-10 bg-[#111111] border-2 border-gray-800 w-[90%] rounded-2xl mt-[40px] border flex flex-col items-center gap-[10px]">
 
           {/*Drag and Drop*/}
-          <div className="pt-10 pb-10 border-2 border-gray-600 border-dotted w-[90%] rounded-2xl flex flex-col justify-center items-center gap-[10px]">
-            <div className="rounded-lg bg-gray-700 w-[90px] h-[90px] flex items-center justify-center">
-              <span className="material-symbols-outlined text-gray-400 scale-200">upload</span>
+          <div className="pt-10 pb-10 border-2 border-gray-600 border-dotted w-[90%] rounded-2xl flex flex-col justify-center items-center gap-[10px] cursor-pointer transition-all duration-300 hover:border-[#3dd6c6] hover:bg-[#151515]">
+            <div className="rounded-lg bg-[#222233] w-[65px] h-[65px] flex items-center justify-center">
+              <span className="material-symbols-outlined text-gray-400 scale-180">upload</span>
             </div>
-            <h2 className="text-white text-xl font-bold">Drag & drop your image</h2>
-            <p className="text-gray-400 text-lg">or <span className="text-[#3dd6c6] font-bold">browse</span> to choose a file</p>
+            <h2 className="text-white text-lg font-bold">Drag & drop your image</h2>
+            <p className="text-gray-400 text-m">or <span className="text-[#3dd6c6] font-bold">browse</span> to choose a file</p>
           </div>
 
           {/*Choose Output Format*/}
@@ -54,14 +55,14 @@ function App() {
           <div className="w-[100%] flex flex-row flex-wrap gap-[30px] justify-center">
             {supportedImageTypes.map(((imageType, key: number) => (
               <span onClick={() => handleFormatChoice(imageType[0])}>
-                <Format key={key} type={imageType[0]} description={imageType[1]} active={imageType[2]}/>   
+                <Format key={key} type={imageType[0]} description={imageType[1]} active={imageType[0] === selectedOutputFormat}/>   
               </span>         
             )))}
           </div>
 
 
           {/*Conversion Button*/}
-          <button className="flex flex-row text-lg bg-[#1e6b63] p-[15px] rounded-xl mt-[30px]">Convert Now <span className="material-symbols-outlined ml-[5px] mt-[3px] scale-80"> arrow_forward </span></button>
+          <button className={`flex flex-row text-lg ${fileUploaded && selectedOutputFormat != "" ? "bg-[#3dd6c6] cursor-pointers" : "bg-[#1e6b63] cursor-not-allowed"} p-[15px] rounded-xl mt-[30px]`}>Convert Now <span className="material-symbols-outlined ml-[5px] mt-[3px] scale-80"> arrow_forward </span></button>
 
         </div>
         <p className="text-gray-400 text-sm">Your files are processed locally and never uploaded to any server</p>
--- src/components/Format.tsx
+++ src/components/Format.tsx
@@ -1,13 +1,14 @@
 interface FormatProp {
     key: number
-    type: string | boolean,
-    description: string | boolean,
-    active: string | boolean
+    type: string,
+    description: string,
+    active: boolean
 }
 
 const Format = ({ key, type, description, active}: FormatProp) => {
-    return <div key={key} className={`bg-${active ? "[#1e6b63]": "[#111111]"} w-[171px] border-${active ? "3 border-[#3dd6c6]" : "2 border-gray-800"} flex flex-col justify-center rounded-lg p-[10px] font-bold`}>
+    return <div key={key} className={`bg-${active ? "[#1e6b63]": "[#111111]"} w-[171px] ${active ? "border-3 border-[#3dd6c6]" : "border-2 border-gray-800"} flex flex-col justify-center rounded-lg p-[10px] font-bold select-none`}>
         <span className="text-[#3dd6c6] text-left font-bold text-lg">{type}</span>
+        {active && <span className="text-[#3dd6c6] scale-200 absolute ml-[145px] mb-[70px]">.</span>}
         <p className="text-gray-400 text-sm font-normal">{description}</p>
     </div>
 }
