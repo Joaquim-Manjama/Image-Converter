ceccc4ea4645bfebb34f215dc81363c37587ef7d
Joaquim Manjama <melorafaelmanjama@gmail.com>
2025-12-14 05:10:57 +0000

    refactoring

--- src/components/Format.tsx
+++ src/components/Format.tsx
@@ -1,3 +1,5 @@
+import COLOR from "../attributes/Colors"
+
 interface FormatProp {
     type: string,
     description: string,
@@ -5,9 +7,9 @@ interface FormatProp {
 }
 
 const Format = ({ type, description, active}: FormatProp) => {
-    return <div className={`bg-${active ? "[#222233]": "[#111111]"} w-[171px] ${active ? "border-3 border-[#33d6c6]" : "border-2 border-gray-800"} flex flex-col justify-center rounded-lg p-[10px] font-bold select-none relative hover:${!active && "bg-[#222222]"}`}>
-        <span className="text-[#3dd6c6] text-left font-bold text-lg">{type}</span>
-        {active && <span className="text-[#3dd6c6] scale-200 absolute ml-[145px] mb-[70px]">.</span>}
+    return <div className={`bg-${active ? "[#222233]": "[#111111]"} w-[171px] ${active ? `border-3 border-${COLOR.blue}` : "border-2 border-gray-800"} flex flex-col justify-center rounded-lg p-[10px] font-bold select-none relative hover:${!active && "bg-[#222222]"}`}>
+        <span className={`text-${COLOR.blue} text-left font-bold text-lg`}>{type}</span>
+        {active && <span className={`text-${COLOR.blue} scale-200 absolute ml-[145px] mb-[70px]`}>.</span>}
         <p className="text-gray-400 text-sm font-normal">{description}</p>
     </div>
 }
